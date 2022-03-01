d201a5e4716b0f788c7378ca505934e5d5823a22
Karolina Guzewska <k.guzewska@gmail.com>
2022-03-01 21:36:31 +0100

    add position for gif

--- stickTheSticker/script.js
+++ stickTheSticker/script.js
@@ -1,12 +1,17 @@
 
-function getImage() {
+function getImage(e) {
 fetch('https://api.giphy.com/v1/stickers/random?api_key=dd9LCJxysHO4ogfqBQmjVceYaW3N9fz1')
   .then(response => response.json())
   .then(response => {
-      const image = document.querySelector("img");
-      image.src = response.data.images.original.url;
+      const body = document.querySelector('body');
+      const newImage = document.createElement("img")
+      newImage.src = response.data.images.original.url;
+      newImage.style.position = "absolute"
+      newImage.style.left = `${e.pageX-(response.data.images.original.width/2)}px`; 
+      newImage.style.top =`${e.pageY-(response.data.images.original.height/2)}px`; 
+      body.appendChild(newImage);
+      console.log(e)
     });
 }
-
-  const body = document.querySelector('body');
-  body.addEventListener("click", getImage);
+const body = document.querySelector('body');
+body.addEventListener("click", getImage);
