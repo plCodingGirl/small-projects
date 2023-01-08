10a1f4d28914e2b3645f7902b780de0eb4906481
Karolina Guzewska <k.guzewska@gmail.com>
2023-01-08 14:13:04 +0100

    add console write how much weight changes

--- howMuchBunny/HowMuchBunny/Program.cs
+++ howMuchBunny/HowMuchBunny/Program.cs
@@ -5,10 +5,15 @@ var weightReadings = new List<Weight>();
 while (true)
 {
     Console.WriteLine("Podaj aktualną wagę królika");
-    var value = Console.ReadLine();
-    weightReadings.Add(new Weight() {Date = DateTime.Now, Value = decimal.Parse(value)});
-    foreach (var weightReading in weightReadings)
+    weightReadings.Add(new Weight() {Date = DateTime.Now, Value = decimal.Parse(Console.ReadLine())});
+    for (var i = 0; i < weightReadings.Count; i++)
     {
-        Console.WriteLine($"{weightReading.Date.ToString("dd-MM-yyyy")} {amelia.Name} {weightReading.Value} kg");
+        Console.Write($"{weightReadings[i].Date.ToString("dd-MM-yyyy")} {amelia.Name} {weightReadings[i].Value} kg ");
+        if (i >= 1)
+        {
+            var howMuch = weightReadings[i].Value-weightReadings[i-1].Value;
+            Console.Write($"zmiana o {howMuch} kg");
+        }
+        Console.WriteLine();
     }
 }
