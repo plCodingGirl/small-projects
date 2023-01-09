75a33d9dc0c312cfbf48a4548d1d63e585837948
Karolina Guzewska <k.guzewska@gmail.com>
2023-01-09 20:13:24 +0100

    add bunnies list

--- howMuchBunny/HowMuchBunny/Program.cs
+++ howMuchBunny/HowMuchBunny/Program.cs
@@ -1,19 +1,34 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Bunny amelia = new Bunny();
-amelia.Name = "helena";
-var weightReadings = new List<Weight>();
+var bunnies = new List<Bunny>();
 while (true)
 {
-    Console.WriteLine("Podaj aktualną wagę królika");
-    weightReadings.Add(new Weight() {Date = DateTime.Now, Value = decimal.Parse(Console.ReadLine())});
-    for (var i = 0; i < weightReadings.Count; i++)
+    Console.WriteLine("Wybierz id królika");
+    Console.WriteLine("1: amelia");
+    Console.WriteLine("2: helena");
+    switch (Console.ReadLine())
     {
-        Console.Write($"{weightReadings[i].Date.ToString("dd-MM-yyyy")} {amelia.Name} {weightReadings[i].Value} kg ");
-        if (i >= 1)
-        {
-            var howMuch = weightReadings[i].Value-weightReadings[i-1].Value;
-            Console.Write($"zmiana o {howMuch} kg");
-        }
-        Console.WriteLine();
+        case "1":
+            Console.WriteLine("Podaj aktualną wagę królika amelia");
+            var bunny = new Bunny() {Name = "amelia", Weight = new List<Weight>() };
+            bunnies.Add(bunny);
+            bunny.Weight.Add(new Weight(){Date = DateTime.Now, Value = decimal.Parse(Console.ReadLine())});
+            break;
+        case "2":
+            Console.WriteLine("Podaj aktualną wagę królika helena");
+            var helena = new Bunny() {Name = "helena", Weight = new List<Weight>() };
+            bunnies.Add(helena);
+            helena.Weight.Add(new Weight(){Date = DateTime.Now, Value = decimal.Parse(Console.ReadLine())});
+            break;
+    }
+    for (var i = 0; i < bunnies.Count; i++)
+    {
+        Console.WriteLine($"{bunnies[i].ToString()}");
+        //Console.WriteLine($"{bunnies[i].Weight[i].Date.ToString("dd-MM-yyyy")} {bunnies[i].Name} {bunnies[i].Weight[i].Value} kg ");
+        //if (i >= 1)
+        //{
+        //    var howMuch = weightReadings[i].Value-weightReadings[i-1].Value;
+        //    Console.Write($"zmiana o {howMuch} kg");
+        //}
+        //Console.WriteLine();
     }
 }
