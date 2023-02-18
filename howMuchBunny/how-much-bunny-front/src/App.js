5d4711d4e41b690b53a10c0f626416c06694c59c
Karolina Guzewska <k.guzewska@gmail.com>
2023-02-18 20:08:28 +0100

    create to-do app

--- howMuchBunny/how-much-bunny-front/src/App.js
+++ howMuchBunny/how-much-bunny-front/src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
+function Todo({ todo }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React js
-        </a>
-      </header>
-    </div>
+      <div className="todo">
+        {todo.text}
+      </div>
+  );
+};
+
+function App() {
+  const [todos, setTodos] = React.useState([
+    { text: "Nikonek" },
+    { text: "Amelionek" },
+    { text: "Helenkonek" }
+  ]);
+
+  return (
+      <div className="app">
+        <div className="todo-list">
+          {todos.map((todo, index) => (
+              <Todo
+                  key={index}
+                  index={index}
+                  todo={todo}
+              />
+          ))}
+        </div>
+      </div>
   );
 }
 
