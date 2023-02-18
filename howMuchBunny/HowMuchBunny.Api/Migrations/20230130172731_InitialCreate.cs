614f5ff4ffc3d7bd8f2d0779ef8502c7b185e7cf
Karolina Guzewska <k.guzewska@gmail.com>
2023-02-18 19:17:26 +0100

    add migrations

---
+++ howMuchBunny/HowMuchBunny.Api/Migrations/20230130172731_InitialCreate.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HowMuchBunny.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Bunnies",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Bunnies", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Weights",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Value = table.Column<decimal>(type: "TEXT", nullable: false),
+                    Date = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    BunnyId = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Weights", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Weights_Bunnies_BunnyId",
+                        column: x => x.BunnyId,
+                        principalTable: "Bunnies",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Weights_BunnyId",
+                table: "Weights",
+                column: "BunnyId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Weights");
+
+            migrationBuilder.DropTable(
+                name: "Bunnies");
+        }
+    }
+}
